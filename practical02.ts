1fedf4163f27c667b1746005c48a23e485edcdd2
avikdas-stack <avikdas.stack@gmail.com>
2026-09-25 10:16:22 -0400

    update practical 02

---
+++ practical02.ts
@@ -0,0 +1,9 @@
+let fullname = "Avik das";
+let age = "20";
+let gender = "Male";
+let phonenumber = "987654312";
+
+console.log(`Full Name:${fullname}`);
+console.log(`Age:${age}`);
+console.log(`Gender:${gender}`);
+console.log(`Phone Number:${phonenumber}`);
